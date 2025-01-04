5ea6bc322e1664755088fe784e9e7735153a614b
Harastani <momoat72@gmail.com>
2025-01-04 09:31:03 +0200

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Route, BrowserRouter, Routes } from "react-router";
-import Item from "./Pages/ItemPage/item.jsx";
+import Item from "./item.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
